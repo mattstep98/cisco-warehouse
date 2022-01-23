ec34942e1fac4f4a6ff2880236ff5f7f22f94622
Matthew Stepnowski <mattstepnowski98@gmail.com>
2022-01-23 12:35:45 -0500

    Implemented Auth
    Login, Logout, Register.
    
    Can only see /hello if logged in through @login_required

--- ciscowarehouse/__init__.py
+++ ciscowarehouse/__init__.py
@@ -1,13 +1,14 @@
 import json
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template,session,redirect,url_for,g
 from flask_mysqldb import MySQL
-
+from . import auth
+import functools
 
 def create_app(test_config=None):
     #create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-
+    app.config.from_mapping(SECRET_KEY='dev')
     #Configuration file
     with open('config.json') as config_file:
         config_data = json.load(config_file)
@@ -18,17 +19,36 @@ def create_app(test_config=None):
 
     mysql = MySQL(app)
 
+
+
+    def login_required(view):
+        @functools.wraps(view)
+        def wrapped_view(**kwargs):
+            if g.user is None:
+                return redirect(url_for('auth.login'))
+
+            return view(**kwargs)
+
+        return wrapped_view
+
+
     #A simple page that says hello
     @app.route('/hello')
+    @login_required
     def hello():
-        return render_template('hello.html',title='HELLO',username='Matt')
+        return render_template('hello.html', title='HELLO', username=session.get('username'))
 
     @app.route('/database')
     def database():
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM ciscowarehouse.inventory;")
+        cursor.execute("SELECT * FROM ciscowarehouse.user;")
         response = cursor.fetchall()
         cursor.close()
         return str(response)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
+
+
---
+++ ciscowarehouse/auth.py
@@ -0,0 +1,81 @@
+from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
+from werkzeug.security import check_password_hash, generate_password_hash
+import mysql.connector
+import json
+
+
+with open('config.json') as config_file:
+        config_data = json.load(config_file)
+mydb = mysql.connector.connect(host=config_data['database']['MYSQL_HOST'], user=config_data['database']['MYSQL_USER'],password=config_data['database']['MYSQL_PASSWORD'],database=config_data['database']['MYSQL_DB'])
+print(mydb)
+mycursor = mydb.cursor(buffered=True)
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        if error is None:
+            try:
+                print("trying")
+                mycursor.execute(
+                    "INSERT INTO user (username, password) VALUES (%s, %s)",
+                    (username, generate_password_hash(password)),
+                )
+                mydb.commit()
+            except:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        #mycursor.execute(("SELECT * FROM user WHERE username = '"+username+"';"))#, (username))
+        mycursor.execute("SELECT * FROM user WHERE user.username = '{}';".format(username))
+        user = mycursor.fetchone()
+        
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user[2], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user[0]
+            session['username'] = user[1]
+            return redirect(url_for('hello'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        mycursor.execute('SELECT * FROM user WHERE id = {}'.format(user_id))
+        g.user = mycursor.fetchone()
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('.login'))
+
